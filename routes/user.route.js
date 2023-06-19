ffe906550ba6bbe3de5913d6868501a52c9c431c
rajesh-fw22-0030 <rajeshkaruppusamy@gmail.com>
2023-06-19 09:06:41 +0530

    backend deployed

--- routes/user.route.js
+++ routes/user.route.js
@@ -49,7 +49,7 @@ userRouter.post("/login", async (req, res) => {
                 if (result) {
                     const token = jwt.sign({ userID: user._id }, process.env.key, { expiresIn: '3h' });
                     const refresh_token = jwt.sign({ userID: user._id }, "R_unlock", { expiresIn: '15h' });
-                    res.send({ msg: "Login successfully", token, refresh_token })
+                    res.send({ msg: "Login successfully", token, refresh_token, userID: user._id })
                 } else {
                     res.send({ message: "Something went wrong, login failed" })
                 }
